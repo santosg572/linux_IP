b41f809a7463598b701dc79778e60e90e7e8b37f
santosg572 <santosg572@gmail.com>
2024-07-29 08:29:58 -0600

    Create comandos.sh

---
+++ comandos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp moca.txt santosg@172.24.220.116:/misc/tournoux2/santosg/linux_IP/etc/network/interfaces1
+scp ridelim.txt santosg@172.24.100.143:/misc/tournoux2/santosg/linux_IP/etc/network/interfaces2
